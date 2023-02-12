91c58a6d1771ef8c35e2f06ec39e45ac908d4c96
smitbry17 <smitbry17@gmail.com>
2023-02-11 20:16:45 -0800

    working on icon hovers

--- portfolio/src/components/AboutCard.jsx
+++ portfolio/src/components/AboutCard.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React,{useState} from "react";
 import img from "../images/aboutImage.JPG";
-import {FaGithub} from "react-icons/fa"
+import {FaGithub, FaLinkedin} from "react-icons/fa"
 
 function AboutCard() {
+  const [showText, setshowText] = useState(false)
   return (
     
     <div>
       {/* About Description */}
-    <div className='flex absolute bg-gray-600 bg-opacity-30 ml-64 h-[30rem] top-64 w-3/5 rounded-lg shadow-xl shadow-black'>
-        <p className='text-slate-100 text-center text-xl font-semibold leading-loose text-opacity-60 w-[60rem]'>
+    <div className='flex fixed bg-gray-600 bg-opacity-30 ml-64 h-[30rem] top-64 w-3/5 rounded-lg shadow-xl shadow-black'>
+        <p className='text-slate-100 fixed mx-5 text-center text-xl font-semibold leading-loose text-opacity-60 w-[55rem]'>
         Hello, My name is Bryan Smith. I am a very self driven person and
             have worked with teams and by myself in many capacities in life. At
             a very young age I started building computers and trying to give
@@ -21,16 +22,24 @@ function AboutCard() {
         </p>
     </div>
     {/* About Image */}
-    <div className='w-1/4 h-1/4 absolute right-56 top-28'>
+    <div className='w-1/4 h-1/4 fixed right-56 top-28'>
         <img className='object-contain rounded-lg shadow-xl shadow-black' src={img} alt="" />
     </div>
 
      {/* Icons */}
-     <div className='flex fixed flex-row top-[60%] left-[50%]'>
-        <a href=''>
-          <FaGithub size={100}/>
+     <div className='flex fixed flex-row top-[60%] left-[40%]'>
+        <a className='hover:bg-slate-100 hover:bg-opacity-60 hover:rounded-full hover:z-10'href='https://github.com/smitbry17' target="_blank"rel="noopener noreferrer">
+          <FaGithub size={130}/>
         </a>
       </div>
+      <div className={!showText ? "hidden" : "opacity-0 hover:opacity-100 duration-300 absolute z-10 inset-0 flex justify-center items-center text-2xl text-white font-semibold"}>
+        LinkedIn
+      </div>
+      <div className="flex fixed flex-row top-[60%] left-[30%]"> 
+      <a className='hover:bg-slate-100 hover:bg-opacity-60 hover:rounded-sm hover:z-10' onMouseEnter={() => setshowText(true) } onMouseLeave={() => setshowText(false)} href=''>
+          <FaLinkedin size={130}/>
+        </a>
+        </div>
 
 </div>
 
