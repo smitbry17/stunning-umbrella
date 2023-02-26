ce55bd69917a0d49fb045fe0788e2c3d83d53e36
smitbry17 <smitbry17@gmail.com>
2023-02-25 22:43:28 -0800

    started on skills animation

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import {SiMysql} from "react-icons/si"
+import { FaHtml5, FaReact } from "react-icons/fa";
 
 function Skills(){
     return(
-        <h1>Hello</h1>
+        <div>
+            {/* sql icon */}
+            <div className='flex sqlicon fixed bg-cyan-700 bg-opacity-30 ml-64 h-[6.5rem] top-64 w-[7.5rem] rounded-lg shadow-xl shadow-black'>
+        <SiMysql className="ml-1" size={110} color="white"/>
+        </div>
+        {/* html icon */}
+        <div className='flex htmlicon fixed bg-orange-600  ml-64 h-[7rem] top-[28rem] w-[7rem] rounded-lg shadow-xl shadow-black'>
+        <FaHtml5 size={110} color="white"/>
+        </div>
+        {/* react icon */}
+        <div className='flex reacticon fixed bg-gray-600 ml-64 h-[7rem] top-[28rem] w-[7.2rem] rounded-lg shadow-xl shadow-black'>
+        <FaReact size={110} color="cyan"/>
+        </div>
+
+        </div>
     )
 }
 
